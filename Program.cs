41992f911744861ec054455bba3bcb4898daaeea
[Samir Jabrayilzada] <[samirej@code.edu.az]>
2022-12-22 16:29:29 +0400

    ProjectForGit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TodayProjectForGIt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
